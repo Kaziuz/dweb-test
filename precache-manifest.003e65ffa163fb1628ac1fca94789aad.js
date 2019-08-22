d312407e75566c212d5a1041d0c7f81ba4c433f7
kaziuz <alex@alex-pc.(none)')>
2019-08-21 23:46:49 -0500

    first commit

---
+++ precache-manifest.003e65ffa163fb1628ac1fca94789aad.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de56ea55f549eda0964712ddfcdae004",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e37421fad28fcf440be",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "1dea9013b3c7da9a6b3c",
+    "url": "/static/css/main.6a5af2ef.chunk.css"
+  },
+  {
+    "revision": "6e37421fad28fcf440be",
+    "url": "/static/js/2.1ae90db8.chunk.js"
+  },
+  {
+    "revision": "1dea9013b3c7da9a6b3c",
+    "url": "/static/js/main.73b0015c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "67d55988d6eb2f8b69571fbff6c36f23",
+    "url": "/static/media/aplicaciones.67d55988.svg"
+  },
+  {
+    "revision": "0be047f5a596563bbac849f2d587a1dc",
+    "url": "/static/media/ecommerce.0be047f5.svg"
+  },
+  {
+    "revision": "c17585781715135b728e0a1858753d17",
+    "url": "/static/media/img1.c1758578.jpg"
+  },
+  {
+    "revision": "e3191f541c072dc361bbd3bf178c264e",
+    "url": "/static/media/img2.e3191f54.jpg"
+  },
+  {
+    "revision": "e4e2171ffaa0e00594a5c91dc96b2ce3",
+    "url": "/static/media/img3.e4e2171f.jpg"
+  },
+  {
+    "revision": "11238a2303386f112a89ed78f9083b69",
+    "url": "/static/media/img4.11238a23.jpg"
+  },
+  {
+    "revision": "a4c6cf37baa2ed1bd09419139b477ab5",
+    "url": "/static/media/img5.a4c6cf37.jpg"
+  },
+  {
+    "revision": "486d5914e085b25b28e0c2c70b0b9a3f",
+    "url": "/static/media/img6.486d5914.jpg"
+  },
+  {
+    "revision": "dbbc026a5203c638d66b66ff201ebb11",
+    "url": "/static/media/img7.dbbc026a.jpg"
+  },
+  {
+    "revision": "5763f84a01a37a8c4f5eeddfaf297570",
+    "url": "/static/media/ux.5763f84a.svg"
+  },
+  {
+    "revision": "7058b543bbebffddbe5be6c661bd6d6e",
+    "url": "/static/media/wordpress.7058b543.svg"
+  }
+]);
